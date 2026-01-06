30211ed0d61bf5df62d3dc1da82082071d979e7e
dmpt1992 <developwebsitedanielamedeira@gmail.com>
2026-01-06 17:00:28 +0000

    Update jscript.js

--- js/jscript.js
+++ js/jscript.js
@@ -1,9 +1,9 @@
 const images = [
-    'img/bg1.jpeg',
-    'img/bg2.jpeg',
-    'img/bg3.jpeg',
-    'img/bg4.jpeg',
-    'img/bg5.jpeg'
+    'img/bg1.jpg',
+    'img/bg2.jpg',
+    'img/bg3.jpg',
+    'img/bg4.jpg',
+    'img/bg5.jpg'
 ];
 
 let current = 0;
